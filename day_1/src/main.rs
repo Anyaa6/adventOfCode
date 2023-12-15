3a8b57956e3465e8443ed047503327d8441116a8
Anyaa6 <ariane.bonnel@gmail.com>
2023-12-15 15:43:17 +0100

    Refactored code

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -5,23 +5,39 @@ use std::env;
 fn get_line_calibration(line: String) -> u32 {
     let mut calibration: u32 = 0;
     
-    for c in line.chars() {
-        if c.is_numeric() {
-            if let Some(digit) = c.to_digit(10) {
-                calibration += digit * 10;
-                break;
-            }
-        }
-    }
-    
-    for c in line.chars().rev() {
-        if c.is_numeric() {
-            if let Some(digit) = c.to_digit(10) {
-                calibration += digit;
-                break;
-            }
-        }
-    }
+    //First digit must be multiplied by 10 so that it creates a double digit number
+    //as asked in the exercise
+    calibration += 10 * line
+    .chars()
+    .find(|c| c.is_numeric())
+    .and_then(|c| c.to_digit(10))
+    .unwrap_or_default();
+
+    calibration += line
+    .chars()
+    .rev()
+    .find(|c| c.is_numeric())
+    .and_then(|c| c.to_digit(10))
+    .unwrap_or_default();
+
+    // // Another less elegant way to solve this exercise
+    // for c in line.chars() {
+    //     if c.is_numeric() {
+    //         if let Some(digit) = c.to_digit(10) {
+    //             calibration += digit * 10;
+    //             break;
+    //         }
+    //     }
+    // }
+
+    // for c in line.chars().rev() {
+    //     if c.is_numeric() {
+        //         if let Some(digit) = c.to_digit(10) {
+    //             calibration += digit;
+    //             break;
+    //         }
+    //     }
+    // }
     
     calibration
 }
